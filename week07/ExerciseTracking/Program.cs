893e1c72c942a8986500a95763efa7ad0fddec71
ecorujoe <osi24005@byui.edu>
2025-06-18 14:29:04 +0100

    ExerciseTracking Completed

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,25 @@
+
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        var activities = new List<Activity>
+        {
+            new Running(new DateTime(2023, 08,06 ), 30, 2.0),
+            new Cycling(new DateTime(2023, 09,03 ), 30, 15.0),
+            new Swimming(new DateTime(2023, 10,08 ), 30, 20),
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
+
     }
 }
